15aa51d27e29df5280aeb271ba58a2ec67ae06e2
sanseref <nicole@sanseref.com>
2016-02-25 10:42:15 -0600

    Move analytics to head instead of js file

--- src/keelsen/wwwroot/js/site.js
+++ src/keelsen/wwwroot/js/site.js
@@ -581,31 +581,3 @@ $(function () {
 
 var random = Math.floor(Math.random() * $('.randomQuote').length);
 $('.randomQuote').hide().eq(random).show();
-
-
-
-
-
-
-
-
-$(function () {
-    $(function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })
-  (window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-74067532-1', 'auto');
-    ga('send', 'pageview');
-
-    setTimeout(function () {
-        var a = document.createElement("script");
-        var b = document.getElementsByTagName("script")[0];
-        a.src = document.location.protocol + "//script.crazyegg.com/pages/scripts/0046/6966.js?" + Math.floor(new Date().getTime() / 3600000);
-        a.async = true; a.type = "text/javascript"; b.parentNode.insertBefore(a, b)
-    }, 1);
-})
-
